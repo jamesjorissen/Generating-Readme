92821c05f7ea50f58cb1eeb5781e0182d8982f4a
James Jorissen <jamesjorissen@gmail.com>
2020-09-12 19:07:33 -0500

    added questions to array

--- index.js
+++ index.js
@@ -4,7 +4,52 @@ const fs = require("fs");
 const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type: "input",
+    message: "Please list your Github username:",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What have you named your project?",
+    name: "projectName"
+},
+{
+    type: "input",
+    message: "What are you calling your repository?",
+    name: "repo"
+},
+{
+    type: "input",
+    message: "Describe your project",
+    name: "projectDescription"
+},
+{
+    type: "list",
+    message: "If your project is licensed - by whom?",
+    name: "license",
+    choices: [
+        "MIT",
+        "Apache",
+        "IBM",
+        "The Perl License"
+    ]
+},
+{
+    type: "input",
+    message: "What command installs dependencies?",
+    name: "installDependencies"
+},
+    , {
+    type: "input",
+    message: "Is there any other information the user needs to know about this repository?",
+    name: "usefulInformation"
+},
+{
+    type: "input",
+    message: "How can others contribute to this repository?",
+    name: "contributions"
+}
 
 ];
 
